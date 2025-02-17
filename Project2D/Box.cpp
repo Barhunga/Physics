20b6b54881f11364846e25e7ef0e52d8e27f880b
Barhunga <mm95x@hotmail.com>
2025-02-17 12:26:12 +1100

    Box draw+fixedupdate
    added draw() and fixedUpdate() implementations and changed float m_width/height to glm::vec2 m_extents

--- Project2D/Box.cpp
+++ Project2D/Box.cpp
@@ -2,4 +2,25 @@
 
 void Box::draw()
 {
+	// if only using rotation
+	// glm::mat4 transform = glm::rotate(m_rotation, glm::vec3(0, 0, 1));
+	// aie::Gizmos::add2DAABBFilled(getCenter(),
+	// m_extents, m_colour, &transform);
+	// draw using local axes
+	glm::vec2 p1 = m_position - m_localX * m_extents.x - m_localY * m_extents.y;
+	glm::vec2 p2 = m_position + m_localX * m_extents.x - m_localY * m_extents.y;
+	glm::vec2 p3 = m_position - m_localX * m_extents.x + m_localY * m_extents.y;
+	glm::vec2 p4 = m_position + m_localX * m_extents.x + m_localY * m_extents.y;
+	aie::Gizmos::add2DTri(p1, p2, p4, m_colour);
+	aie::Gizmos::add2DTri(p1, p4, p3, m_colour);
+}
+
+void Box::fixedUpdate(glm::vec2 gravity, float timeStep)
+{
+	RigidBody::fixedUpdate(gravity, timeStep);
+	//store the local axes
+	float cs = cosf(m_orientation);
+	float sn = sinf(m_orientation);
+	m_localX = glm::normalize(glm::vec2(cs, sn));
+	m_localY = glm::normalize(glm::vec2(-sn, cs));
 }
--- Project2D/Box.h
+++ Project2D/Box.h
@@ -6,18 +6,22 @@ class Box : public RigidBody
 public:
     Box(glm::vec2 position, glm::vec2 velocity,
         float mass, float width, float height, glm::vec4 colour) :
-        m_width(width), m_height(height), m_colour(colour), RigidBody(BOX, position, velocity, 0, mass,
-            1.0f / 12.0f * m_mass * m_width * m_height, 0) {
+        m_extents(width, height), m_colour(colour), RigidBody(BOX, position, velocity, 0, mass,
+            1.0f / 12.0f * m_mass * (m_extents.x * 2) * (m_extents.y * 2), 0) {
     }
     ~Box() {}
 
     virtual void draw() override;
+    virtual void fixedUpdate(glm::vec2 gravity, float timeStep) override;
 
     glm::vec4 getColour() { return m_colour; }
 
 protected:
-    float m_width;
-    float m_height;
+    glm::vec2 m_extents; // the halfedge lengths
     glm::vec4 m_colour;
+
+    // store the local x,y axes of the box based on its angle of rotation
+    glm::vec2 m_localX;
+    glm::vec2 m_localY;
 };
 
